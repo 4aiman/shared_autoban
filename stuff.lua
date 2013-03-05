20d06104827712de0f2bbe5c07478f4bf460b1e9
4aiman <4aiman@inbox.ru>
2013-03-05 19:51:14 +0400

    just to stay atop

---
+++ stuff.lua
@@ -0,0 +1,10 @@
+
+bans = minetest.deserialize('return { ["singleplayer"] = { ["data"] = { { ["own"] = "4", ["cou"] = 22 }, { ["own"] = "4aiman", ["cou"] = 24 }, { ["own"] = "4aiman2", ["cou"] = 0 }, { ["own"] = "4ai", ["cou"] = 4 } } } }')
+
+ex_pos = minetest.deserialize('return {  }')
+
+trusted = minetest.deserialize('return {  }')
+
+exceptions = minetest.deserialize('return {  }')
+
+votes = minetest.deserialize('return {  }')
